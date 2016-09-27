df3d1f067da3f0925522e1e68550a6da1b16da5f
Leonardo Maran <leonardogmaran9@gmail.com>
2016-09-27 11:38:07 -0300

    Projeto - Financas

---
+++ Financas/Controllers/UsuarioController.cs
@@ -0,0 +1,63 @@
+﻿using Financas.DAO;
+using Financas.Entidades;
+using Financas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using WebMatrix.WebData;
+
+namespace Financas.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private UsuarioDAO usuarioDAO;
+        public UsuarioController(UsuarioDAO usuarioDAO)
+        {
+            this.usuarioDAO = usuarioDAO;
+        }
+        public ActionResult Form()
+        {
+            return View();
+        }
+
+        public ActionResult Adiciona(UsuarioModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    Usuario usuario = new Usuario
+                    {
+                        Nome = model.Nome,
+                        Email = model.Email
+                    };
+                    usuarioDAO.Adiciona(usuario);
+                    WebSecurity.CreateUserAndAccount(model.Nome, model.Senha,
+                        new { Email = model.Email });
+                    return RedirectToAction("Index");
+
+             
+                }
+                catch (MembershipCreateUserException e)
+                {
+                    ModelState.AddModelError("usuário.Invalido", e.Message);
+                    return View("Form", model);
+                }
+            }
+            else
+            {
+                return View("Form", model);
+            }
+        }
+        public ActionResult Index()
+        {
+            IList<Usuario> usuarios = usuarioDAO.Lista();
+            return View(usuarios);
+        }
+    }
+}
+
+    
---
+++ Financas/DAO/MovimentacaoDAO.cs
@@ -0,0 +1,65 @@
+﻿using Financas.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Financas.DAO
+{
+    public class MovimentacaoDAO
+    {
+        private FinancasContext context;
+
+        public decimal DataMinima { get; private set; }
+
+        public MovimentacaoDAO(FinancasContext context)
+        {
+            this.context = context;
+        }
+        public void Adiciona(Movimentacao movimentacao)
+        {
+            context.Movimentacoes.Add(movimentacao);
+            context.SaveChanges();
+        }
+        public IList<Movimentacao> Lista()
+        {
+            return context.Movimentacoes.ToList();
+        }
+
+        public IList<Movimentacao> BuscaPorUsuario(int? usuarioId)
+        {
+            return context.Movimentacoes.Where(m => m.UsuarioId == usuarioId).ToList();
+        }
+
+        public IList<Movimentacao> Busca(decimal? ValorMinimo, decimal? ValorMaximo,
+            DateTime? DataMinima, DateTime? DataMaxima, Tipo? tipo, int? UsuarioId)
+        {
+            IQueryable<Movimentacao> busca = context.Movimentacoes;
+            if(ValorMinimo.HasValue)
+            {
+                busca = busca.Where(m => m.Valor >= ValorMinimo);
+            }
+            if(ValorMaximo.HasValue)
+            {
+                busca = busca.Where(m => m.Valor <= ValorMaximo);
+            }
+            if(DataMinima.HasValue)
+            {
+                busca = busca.Where(m => m.Data <= DataMinima);
+            }
+            if(DataMaxima.HasValue)
+            {
+                busca = busca.Where(m =>m.Data <= DataMaxima);
+            }
+            if(tipo.HasValue)
+            {
+                busca = busca.Where(m => m.Tipo == tipo);
+            }
+            if(UsuarioId.HasValue)
+            {
+                busca = busca.Where(m => m.UsuarioId == UsuarioId);
+            }
+            return busca.ToList();
+        }
+    }
+}
